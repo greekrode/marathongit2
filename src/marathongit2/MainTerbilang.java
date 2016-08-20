00a7fe9a86481f6a5dbd506c668d01a91e107a3a
Roderick Halim <roderickhalim@gmail.com>
2016-08-20 14:30:50 +0700

    Add feature terbilang

---
+++ src/marathongit2/MainTerbilang.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marathongit2;
+
+import static marathongit2.Terbilang.proses;
+/**
+ *
+ * @author roder
+ */
+public class MainTerbilang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(proses(24700));
+    }
+    
+}
